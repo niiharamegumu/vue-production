ac64e713bf34955166327fcf71b55fd98aa5ced6
niiharamegumu <nhrmgm1223@gmail.com>
2018-10-18 17:33:33 +0900

    add localeStorageAPI.

--- todoList/main.js
+++ todoList/main.js
@@ -1,9 +1,27 @@
 'use strict';
 
+var STORAGE_KEY = 'todos-vuejs-demo';
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    );
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+};
+
+
 var todo = new Vue({
 	el: '#todo',
 	data: {
-		
+
 	},
 	method: {
 
