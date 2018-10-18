4843f3546a4c95cdf066edd260e07382863b22e3
niiharamegumu <nhrmgm1223@gmail.com>
2018-10-18 23:50:29 +0900

    create sample.

--- todoList/main.js
+++ todoList/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var STORAGE_KEY = 'todos-vuejs-demo';
+var STORAGE_KEY = 'todo-vuejs';
 var todoStorage = {
   fetch: function() {
     var todos = JSON.parse(
@@ -21,9 +21,57 @@ var todoStorage = {
 var todo = new Vue({
 	el: '#todo',
 	data: {
-
+		todos: [],
+		options: [
+			{ value: -1, label: 'すべて' },
+			{ value: 0, label: '作業中' },
+			{ value: 1, label: '完了' }
+		],
+		current: -1
 	},
-	method: {
+	computed: {
+		computedTodos: function(){
+			return this.todos.filter(function(el){
+				return this.current < 0 ? true : this.current === el.state;
+			},this);
+		},
+		labels: function(){
+			return this.options.reduce(function (a, b) {
+				return Object.assign(a, { [b.value]: b.label });
+			}, {});
+		}
+	},
+	watch: {
+		todos: {
+			handler: function(todos){
+				todoStorage.save(todos);
+			},
+			deep: true
+		}
+	},
+	created: function() {
+		this.todos = todoStorage.fetch();
+	},
+	methods: {
+		doAdd: function(event, value){
+			var comment = this.$refs.comment;
+			if(!comment.value.length){
+				return;
+			}
+			this.todos.push({
+				id: todoStorage.uid++,
+				comment: comment.value,
+				state: 0
+			});
+			comment.value = '';
+		},
+		doChangeState: function(item){
+			item.state = item.state ? 0 : 1;
+		},
+		doRemove: function(item){
+			var index = this.todos.indexOf(item);
+			this.todos.splice(index, 1);
+		}
 
 	}
 });
