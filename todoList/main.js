25a3e4d10033d2b76e16982c32ad607d095aeaf9
niiharamegumu <nhrmgm1223@gmail.com>
2018-10-24 22:13:33 +0900

    Easy fix.

--- todoList/main.js
+++ todoList/main.js
@@ -2,19 +2,19 @@
 
 var STORAGE_KEY = 'todo-vuejs';
 var todoStorage = {
-  fetch: function() {
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    );
-    todos.forEach(function(todo, index) {
-      todo.id = index;
-    });
-    todoStorage.uid = todos.length;
-    return todos;
-  },
-  save: function(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
+	fetch: function() {
+		var todos = JSON.parse(
+			localStorage.getItem(STORAGE_KEY) || '[]'
+		);
+		todos.forEach(function(todo, index) {
+			todo.id = index;
+		});
+		todoStorage.uid = todos.length;
+		return todos;
+	},
+	save: function(todos) {
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+	}
 };
 
 
